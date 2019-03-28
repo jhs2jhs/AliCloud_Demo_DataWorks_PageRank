5618b128c637e819247ddc3938ae4f3fe00c9ecd
Jianhua Shao <jianhua.shao1986@gmail.com>
2019-03-28 22:45:35 +0000

    Update sql_ddl_dwd_pagerank.sql

--- sql_ddl_dwd_pagerank.sql
+++ sql_ddl_dwd_pagerank.sql
@@ -1,6 +1,24 @@
+-- run pai to get pagerank result
+drop table if exists dwd_pagerank;
+
+PAI -name PageRankWithWeight
+    -project algo_public
+    -DinputEdgeTableName=ods_pagerank
+    -DfromVertexCol=from_user
+    -DtoVertexCol=to_user
+    -DoutputTableName=dwd_pagerank
+    -DhasEdgeWeight=true
+    -DedgeWeightCol=weight
+    -DmaxIter 100;
+
+
+-- add time information to sync out
+drop table if exists dwd_pagerank_out;
+
 create table if not exists dwd_pagerank_out (
     user string,
-    weight double
+    weight double,
+    ds string
 );
 
--- ignore partition in this demo
+insert into dwd_pagerank_out (user, weight, ds) select *, getdate() from dwd_pagerank;
